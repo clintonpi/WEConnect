e758670ea394581d959a93bddb8331a77e727481
clintonpi <nkwochaclinton@gmail.com>
2018-03-14 14:46:49 +0100

    Feature: Implement signin and sign up for users

---
+++ app.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import validator from 'express-validator';
+import bodyParser from 'body-parser';
+import userRouter from './server/fakeServer/routes/userRoute';
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(validator());
+
+app.get('/', (req, res) => {
+  res.status(200).send('Welcome to WEconnect Mock-data Api');
+});
+
+app.use('/api/v1/auth', userRouter);
+
+const port = process.env.PORT || 4000;
+
+app.listen(port, () => {
+  console.log(`Connected on port ${port}`);
+});
+
+export default app;
