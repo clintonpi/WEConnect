7ad92964c273cb0f156aa8d9957483bf523a18f1
clintonpi <nkwochaclinton@gmail.com>
2018-03-15 14:41:25 +0100

    Chore: Remove unnecessary console.log statement

--- app.js
+++ app.js
@@ -19,8 +19,6 @@ app.use('/api/v1', businessRouter);
 
 const port = process.env.PORT || 4000;
 
-app.listen(port, () => {
-  console.log(`Connected on port ${port}`);
-});
+app.listen(port);
 
 export default app;
