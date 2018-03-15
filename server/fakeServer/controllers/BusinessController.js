e9b9a1d234463da675935fbcf2f329e18e329bc1
clintonpi <nkwochaclinton@gmail.com>
2018-03-15 21:33:18 +0100

    Feature: Implement "update business profile" feature

--- server/fakeServer/controllers/BusinessController.js
+++ server/fakeServer/controllers/BusinessController.js
@@ -4,7 +4,7 @@ const { business } = fakeDb;
 
 /**
  * @class BusinessController
- * @classdesc implements user being able to register a business
+ * @classdesc Implements user being able to register a business and update business profile
  */
 class BusinessController {
   /**
@@ -13,7 +13,7 @@ class BusinessController {
    * @static
    * @param {object} req - The request object
    * @param {object} res - The response object
-   * @return {object} Message and user data
+   * @return {object} Message and business data
    * @memberof BusinessController
    */
   static registerBusiness(req, res) {
@@ -32,6 +32,40 @@ class BusinessController {
     res.status(201).json({
       message: 'Business registered successfully!',
       data: business[business.length - 1]
+    });
+  }
+
+  /**
+   * Update Business Profile
+   *
+   * @static
+   * @param {object} req - The request object
+   * @param {object} res - The response object
+   * @return {object} Message and updated business data
+   * @memberof BusinessController
+   */
+  static updateBusinessProfile(req, res) {
+    const {
+      name, location, category, description, userId
+    } = req.body;
+    const businessId = parseInt(req.params.id, 10);
+    const editedBusinessProfile = {
+      id: businessId,
+      name,
+      location,
+      category,
+      description,
+      userId
+    };
+    business.forEach((bus) => {
+      if (bus.id === businessId) {
+        const businessIndex = business.indexOf(bus);
+        business.splice(businessIndex, 1, editedBusinessProfile);
+      }
+    });
+    res.status(200).json({
+      message: 'Business profile updated successfully!',
+      data: editedBusinessProfile
     });
   }
 }
--- server/fakeServer/routes/businessRoute.js
+++ server/fakeServer/routes/businessRoute.js
@@ -1,11 +1,14 @@
 import express from 'express';
 import business from '../controllers/BusinessController';
 
-const { registerBusiness } = business;
+const { registerBusiness, updateBusinessProfile } = business;
 
 const businessRouter = express.Router();
 
 businessRouter.route('/businesses')
   .post(registerBusiness);
+
+businessRouter.route('/businesses/:id')
+  .put(updateBusinessProfile);
 
 export default businessRouter;
