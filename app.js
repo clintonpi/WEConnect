54a15c4d16eefc8050620c2772878435d8aa7077
clintonpi <nkwochaclinton@gmail.com>
2018-04-12 11:45:15 +0100

    Chore: Uninstall irrelevant packages

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 import express from 'express';
-import validator from 'express-validator';
 import bodyParser from 'body-parser';
 import userRouter from './server/fakeServer/routes/userRoute';
 import businessRouter from './server/fakeServer/routes/businessRoute';
@@ -9,7 +8,6 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(validator());
 
 app.use('/api/v1/auth', userRouter);
 app.use('/api/v1', businessRouter, reviewRouter);
